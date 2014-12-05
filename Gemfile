2e66ab599ae6c0855881502b72cada3d9882177c
Iryna Ko <miss.iryna@gmail.com>
2014-12-05 23:19:37 +0200

    Add SSL in production1

--- Gemfile
+++ Gemfile
@@ -58,5 +58,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
 group :production do
   gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
+ gem 'bcrypt-ruby', '3.1.2', :require => 'bcrypt'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,13 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20141119150025) do
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+    create_table "users", force: true do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-
+    t.string "password_digest"
+end
+    add_index "users", ["email"], name: "index_users_on_email", unique: true
 end
